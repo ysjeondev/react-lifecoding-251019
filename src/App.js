2c4b07b6481d626639e1c9e7d0bd553be07dfefe
ysjeondev <hpc9909@gmail.com>
2025-10-22 22:13:12 +0900

    react 생활코딩 58page 까지 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Header(props){
+  console.log('props',props)
+  return(
+    <header>
+      <h1><a href="/">{props.title}</a></h1>
+    </header>
+  )
+}
+
+function Nav(props){
+  const lis=[]
+      for(let i=0;i<props.topics.length;i++){
+        let t=props.topics[i];
+        lis.push(<li>{t.title}</li>)
+      }
+  return(
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  )
+}
+
+function Article(props){
+  return(
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  )
+}
+
+
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'htms is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'}
+  ]
   return (
-    <div className="App">
-       Hello React
+    <div >
+      <Header title="REACT"></Header>      
+       <Nav topics={topics}></Nav>
+       <Article title="Welcome" body="Hello, Web"></Article>
+       
     </div>
   );
 }
