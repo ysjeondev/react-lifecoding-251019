00540fdea47c43c08e279420c23e7f6d7047a4aa
ysjeondev <hpc9909@gmail.com>
2025-10-24 16:34:11 +0900

    react 생활코딩 64쪽까지 완료

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function Header(props){
   console.log('props',props)
   return(
     <header>
-      <h1><a href="/">{props.title}</a></h1>
+      <h1><a href="/" onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
     </header>
   )
 }
@@ -15,7 +18,12 @@ function Nav(props){
   const lis=[]
       for(let i=0;i<props.topics.length;i++){
         let t=props.topics[i];
-        lis.push(<li>{t.title}</li>)
+        lis.push(<li key={t.id}>
+          <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+            event.preventDefault();
+            props.onChangeMode(event.target.id);
+          }}>{t.title}</a>
+        </li>)
       }
   return(
     <nav>
@@ -44,9 +52,15 @@ function App() {
   ]
   return (
     <div >
-      <Header title="REACT"></Header>      
-       <Nav topics={topics}></Nav>
-       <Article title="Welcome" body="Hello, Web"></Article>
+      <Header title="WEB" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>      
+
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
+      
+      <Article title="Welcome" body="Hello, Web"></Article>
        
     </div>
   );
