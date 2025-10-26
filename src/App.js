7da17284929c0c5b737582832061e50c54d67ff4
ysjeondev <hpc9909@gmail.com>
2025-10-26 16:13:11 +0900

    react 생활코딩 79쪽까지 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 
 function Header(props){
@@ -21,7 +22,7 @@ function Nav(props){
         lis.push(<li key={t.id}>
           <a id={t.id} href={'/read/'+t.id} onClick={event=>{
             event.preventDefault();
-            props.onChangeMode(event.target.id);
+            props.onChangeMode(Number(event.target.id));
           }}>{t.title}</a>
         </li>)
       }
@@ -45,22 +46,46 @@ function Article(props){
 
 
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+
+  console.log('_mode', mode)
+  //const mode='READ';
+
   const topics = [
     {id:1, title:'html', body:'htms is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
   ]
+  
+  let content =null;
+
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, Web"></Article>
+  }else if(mode === 'READ'){
+    let title, body=null;
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id ) {
+        title =topics[i].title;
+        body= topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+
+  }
+
   return (
     <div >
       <Header title="WEB" onChangeMode={()=>{
-        alert('Header');
+        setMode('WELCOME')
       }}></Header>      
 
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ')
+        setId(_id)
       }}></Nav>
       
-      <Article title="Welcome" body="Hello, Web"></Article>
+      {content}
        
     </div>
   );
